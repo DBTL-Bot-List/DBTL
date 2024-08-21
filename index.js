5797d35a3466c52ffcd7997040249bc2933635c5
Uxoi <jhojanproxdd@gmail.com>
2024-08-21 18:55:53 -0500

    Update Index.js

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ function loadRoutes(basePath, baseRoute) {
 
 loadRoutes(routesPath, '');
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+})
+
 app.listen(3000, () => {
     console.log(`Ready!!!`);
 });
